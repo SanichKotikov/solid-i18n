5879d7e9435d480744406235fb21f4e3a7af7f00
RoachxD <nicolae.adrian.m@gmail.com>
2023-08-15 13:16:55 +0300

    feat: add `index.ts` to `components` and update main `index.ts`
    - Created `index.ts` file for `components`.
    - Updated main `index.ts` in order to reflect the addition.
    - Added `I18nOptions` type export.

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,14 @@ import { createI18n as create } from 'i18n-mini';
 import type { I18nOptions } from 'i18n-mini';
 import type { I18n } from './types';
 
+export type { I18n, I18nOptions };
+
 export { defineMessages } from 'i18n-mini';
 export type { I18nPresets } from 'i18n-mini';
 
-export { I18nProvider } from './components/I18nProvider';
-export { Text } from './components/Text';
-export { Numeric } from './components/Numeric';
-export { DateTime } from './components/DateTime';
+export * from './components';
 export { useI18n } from './context';
 
-export type { TextProps } from './components/Text';
-export type { NumericProps } from './components/Numeric';
-export type { DateTimeProps } from './components/DateTime';
-export type { I18n } from './types';
 
 function formatTag(tag: string, child: string | string[] | undefined) {
   const el = template(`<${tag}>`);
